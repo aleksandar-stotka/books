3c2d1ac7e522982ad8f25a4efc84375ec0da6a20
aleksandar angelevski <stotka123@yahoo.com>
2022-06-22 15:32:21 +0200

    fetc-books

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import BookItems from "./BooksItem";
+function App() {
+  return (
+    <div>
+      <Home />
+      <BookItems />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/BooksItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState, useEffect } from "react";
+const urlBook =
+  "https://www.googleapis.com/books/v1/volumes?q=react&key=AIzaSyCugL3EmH4MtIyW--UPxSimPKWjIA5KmAg";
+
+const BookItems = () => {
+  const [books, setBooks] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  ////fetch
+  const fetchBooks = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(urlBook);
+      const data = await response.json();
+      setBooks(data);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  return <div>book items</div>;
+};
+
+export default BookItems;
