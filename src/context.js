cf7e1d0462d8d4c891a32c8ce8f73de7f0dd544b
aleksandar angelevski <stotka123@yahoo.com>
2022-06-22 20:08:39 +0200

    fetch-items-done

--- src/context.js
+++ src/context.js
@@ -10,7 +10,44 @@ const AppProvider = ({ children }) => {
 
   const fetchBooks = useCallback(async () => {
     setLoading(true);
-  });
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/books/v1/volumes?q=book&key=AIzaSyCugL3EmH4MtIyW--UPxSimPKWjIA5KmAg"
+      );
+      const data = await response.json();
+      console.log(data);
+      //////////////////////////////////
+      const { items } = data;
+
+      if (items) {
+        const newBooks = items.map((item) => {
+          const { description, title, averageRating } = item.volumeInfo;
+          const { id, selfLink } = item;
+
+          return {
+            id: id,
+            title: title,
+            desc: description,
+            link: selfLink,
+            raiting: averageRating,
+          };
+        });
+        setBooks(newBooks);
+        console.log(newBooks);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, searchTerm);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  return <AppContext.Provider value="heloo">{children}</AppContext.Provider>;
+};
+export const useGlobalContext = () => {
+  return useContext(AppContext);
 };
 
 export { AppContext, AppProvider };
